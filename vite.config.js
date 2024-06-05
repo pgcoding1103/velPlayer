5519a1f9f9a51382d1a3cca79c91d8c07293ab0d
pgcoding1103 <chenpanggui@163.com>
2024-06-05 14:45:30 +0800

    build: 引入postcss-pxtorem

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+//引入postcss
+import postcss from 'postcss-pxtorem'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -18,6 +20,16 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': '/src'
+    }
+  },
+  css: {
+    postcss: {
+      plugins: [
+        postcss({
+          rootValue: 37.5,
+          propList: ['*']
+        })
+      ]
     }
   }
 })
